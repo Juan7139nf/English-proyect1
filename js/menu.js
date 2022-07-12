bf6d4716ad0e372ffab67a1ab96097e3a1c3ab3e
Juan7139nf <109113501+Juan7139nf@users.noreply.github.com>
2022-07-12 12:42:40 -0400

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const hamburguer = document.querySelector('.button');
+const menu = document.querySelector('.nav');
+
+console.log(menu)
+console.log(hamburguer)
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburguer ){
+
+        menu.classList.toggle('spread')
+
+    }
+})
